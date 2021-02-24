40af21c8fb9cdfb179ce40f93beb4289ba1604e3
Amer Hamdan <Amer.Hamdan9@T-Mobile.com>
2021-02-23 21:50:04 -0800

    Added launch status

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -19,9 +19,18 @@ export default class Video extends Component {
             className += " video-collapsed";
         } else {
             const videoId = getVideoId(selectedVideo.links.video_link);
+            let successClass = "content";
+
+            if (selectedVideo.launch_success) {
+                successClass += " successful-launch";
+            } else {
+                successClass += " failed-launch";
+            }
+
             content = <div>
                 <iframe className="video-frame" src={`https://www.youtube.com/embed/${videoId}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <p>{selectedVideo.details ? selectedVideo.details : "No details available for this video"}</p>
+                <p className="content">{selectedVideo.details ? selectedVideo.details : "No details available for this video"}</p>
+                <p className={successClass}>{selectedVideo.launch_success ? "Launch successful" : "Launch did not succeed"}</p>
                 <button className="button is-danger details-close" onClick={this.setSelectedVideo}>Close</button>
             </div>
         }
